7ba0f42e6a30152231a491a03d0c874b3516711f
Harryson Pointdujour <Harryson Pointdujour>
2016-12-01 18:44:14 -0500

    created course model, added mongoose to index.js, verified file has no syntax errors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var server = express();
+var mongoose = require('mongoose');
 
 var port = process.env.PORT || 8080;
+var mongoURI = process.env.MONGOURI || require('./config.js').mongoURI;
 
 server.use(express.static(__dirname + '/public'));
 
+mongoose.connect(mongoURI);
 
 server.get('/', function(req, res){
   res.sendFile('public/html/index.html', {root:__dirname});
--- server/models/course.model.js
+++ server/models/course.model.js
@@ -1,1 +1,25 @@
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var courseSchema = new Schema({
+  title:{
+    type: String,
+    required: true
+  },
+  description:{
+    type: String,
+    required: true
+  },
+  author:{
+    type: String, //this will change
+    required: true
+  },
+  postDate:{
+    type: Date,
+    required:true
+  },
+  links: [{}]
+});
+
+var Course = mongoose.model('Course', courseSchema);
+module.exports = Course;
