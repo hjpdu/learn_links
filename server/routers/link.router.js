1105eeca26584fb860d555a4517d36735854db12
Harryson Pointdujour <Harryson Pointdujour>
2016-12-02 18:03:45 -0500

    working on link router

--- server/routers/link.router.js
+++ server/routers/link.router.js
@@ -2,7 +2,15 @@ var express = require('express');
 var router = express.Router();
 var Link = require('../models/link.model.js');
 
-router.get('/links', function(req, res){});
+router.get('/links', function(req, res){
+  Link.find({}, function(err, links){
+    if(err){
+      return res.status(500).json({
+        msg: err
+      });
+    }
+  });
+});
 router.post('/links',function(req, res){});
 router.put('/links/:id', function(req, res){});
 router.delete('/links/:id', function(req, res){});
