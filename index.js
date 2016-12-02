39bf76987952d3654bdfe484f1e9665e4df800cb
Harryson Pointdujour <Harryson Pointdujour>
2016-12-01 19:34:34 -0500

    finished adding body parser url encoded to index.js

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 var express = require('express');
 var server = express();
 var mongoose = require('mongoose');
+var courseRouter = require('./server/routers/course.router.js');
+var bodyParser = require('body-parser');
 
 var port = process.env.PORT || 8080;
 var mongoURI = process.env.MONGOURI || require('./config.js').mongoURI;
 
 server.use(express.static(__dirname + '/public'));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended:true}));
 
 mongoose.connect(mongoURI);
 
 server.get('/', function(req, res){
   res.sendFile('public/html/index.html', {root:__dirname});
 });
+server.use('courseRouter');
 
 server.listen(port, function(){
   console.log('Now listening on port...', port);
