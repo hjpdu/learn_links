8646b2e216b8625f0adfa63720d16764fea27ec5
Harryson Pointdujour <Harryson Pointdujour>
2016-12-02 20:13:35 -0500

    implemented post route for link

--- server/routers/link.router.js
+++ server/routers/link.router.js
@@ -17,6 +17,16 @@ router.get('/links', function(req, res){
 router.post('/links',function(req, res){
   var link = new Link(req.body);
   link.postDate = new Date();
+  link.save(function(err){
+    if(err){
+      return res.status(500).json({
+        msg: err
+      });
+    }
+    return res.status(200).json({
+      msg: 'Nice. Your link has been added to the course!'
+    });
+  });
 });
 router.put('/links/:id', function(req, res){});
 router.delete('/links/:id', function(req, res){});
