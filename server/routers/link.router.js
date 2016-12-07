2fa61cba1ba2bd2c14c68fd813ea7bcc4513b2c5
Harryson Pointdujour <Harryson Pointdujour>
2016-12-06 20:33:34 -0500

    randm push while justin helps

--- server/routers/link.router.js
+++ server/routers/link.router.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Link = require('../models/link.model.js');
+var Course = require('../models/course.model.js');
 
 router.get('/links', function(req, res){
   Link.find({}, function(err, links){
@@ -14,19 +15,21 @@ router.get('/links', function(req, res){
     });
   });
 });
-router.post('/links',function(req, res){
+router.post('/links/',function(req, res){
   var link = new Link(req.body);
   link.postDate = new Date();
-  link.save(function(err){
-    if(err){
-      return res.status(500).json({
-        msg: err
-      });
-    }
-    return res.status(200).json({
-      msg: 'Nice! Your link has been added to the course.'
-    });
-  });
+  
+
+  // link.save(function(err){
+  //   if(err){
+  //     return res.status(500).json({
+  //       msg: err
+  //     });
+  //   }
+  //   return res.status(200).json({
+  //     msg: 'Nice! Your link has been added to the course.'
+  //   });
+  // });
 });
 router.put('/links/:id', function(req, res){});
 router.delete('/links/:id', function(req, res){});
